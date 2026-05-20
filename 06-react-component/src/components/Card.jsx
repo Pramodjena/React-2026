687505df72205b4eed94e54af2baffb9b9e3c146
Pramodjena <pramoodjena@gmail.com>
2026-05-20 14:08:24 +0530

    react props updated

--- 06-react-component/src/components/Card.jsx
+++ 06-react-component/src/components/Card.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card-container">
-      <img
-        src="https://media.licdn.com/dms/image/v2/D5603AQHAP4gynqQkgQ/profile-displayphoto-scale_200_200/B56Z4vY8fuGoAc-/0/1778911531392?e=1780531200&v=beta&t=_1wGK-c7RvF_OL6ubuGfi4HDMpP54FmEhu20qdc9o4w"
-        alt="profile_pic"
-      />
-      <h2>Name: Pramod Kumar Jena</h2>
-      <p>DOB: 21.08.1110</p>
-      <button>Follow</button>
+      <img src={props.src} alt={props.alt} />
+      <h2>Name:{props.name} </h2>
+      <p>DOB: {props.dob} </p>
+      <button onClick={props.click}>Follow</button>
     </div>
   );
 };
