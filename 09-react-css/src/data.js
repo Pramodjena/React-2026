999c8b8650891e06c872a65f770bdbdb8ceb27e0
Pramodjena <pramoodjena@gmail.com>
2026-05-28 11:21:08 +0530

    react-ui(tailwind css)

---
+++ 09-react-css/src/data.js
@@ -0,0 +1,40 @@
+const users = [
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQHAP4gynqQkgQ/profile-displayphoto-scale_400_400/B56Z4vY8fuGoAg-/0/1778911531392?e=1781740800&v=beta&t=ZjeOnFiauDRgvbhL9bs4EUHgXlNtXQMhgzWmEGo3ITg",
+    intro: "",
+    color: "royalblue",
+    tag: "CTO",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQEYsIU0KphAyg/profile-displayphoto-scale_400_400/B56ZiDU8kkHkAg-/0/1754549964122?e=1781740800&v=beta&t=GcyxytiOjRVv07ymfeLrl1ZtZ5RY-9QNCeogeBin3ts",
+    color: "lightseagreen",
+    intro: "",
+    tag: "Chairman",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQH1P3q7h98SNw/profile-displayphoto-scale_400_400/B56Z43Q1wJGgAg-/0/1779043624171?e=1781740800&v=beta&t=7KnaokFrqjgWP0hd5R6sZf4TWK4bfuvx_mBjnaj0g_o",
+    color: "orange",
+    intro: "",
+    tag: "Oeartion Head",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQG6o_Wcmf2ASg/profile-displayphoto-shrink_400_400/B56ZRWdpvQH0Ag-/0/1736617402895?e=1781740800&v=beta&t=fauvB7do9Y-zydqAe1D0Ycia92DRZBTwAuPUCt270To",
+    color: "purple",
+    intro: "",
+    tag: "Vice-President",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D4D03AQFWGRhqEohbyw/profile-displayphoto-scale_400_400/B4DZoQ493BGQAk-/0/1761219993832?e=1781740800&v=beta&t=9rYXydcLP0AglW8J2ahihPM6-J_Z5syF8FrAFangwio",
+    intro: "",
+    color: "black",
+    tag: "President",
+  },
+  {
+    img: "https://media.licdn.com/dms/image/v2/D5603AQEOCPURwI78qg/profile-displayphoto-scale_400_400/B56ZuUNKeOG4Ag-/0/1767718039696?e=1781740800&v=beta&t=C6mE7eeoKf2P-aGTezJX9W5bzN44a8-ymLcKstE7o_Y",
+    intro: "",
+    color: "orange",
+    tag: "CMO",
+  },
+];
+
+export default users;
